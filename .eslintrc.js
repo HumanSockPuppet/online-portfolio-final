13ab3ac3e7f63bf604b767ac89718766597c9782
adimapilis <dimapilis.adrian08@gmail.com>
2024-03-28 15:30:20 +0800

    chore: setup prettier
    JIRA-2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,24 @@
 // .eslintrc.js example
 // eslint-disable-next-line no-undef
-module.exports ={
-  "env": {
-    "browser": true,
-    "es2021": true
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended"],
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  "plugins": ["react", "react-hooks"],
-  "rules": {
+  plugins: ["react", "react-hooks"],
+  rules: {
     //add customize rules here as per your project's needs
-  }
-}  
+  },
+};
