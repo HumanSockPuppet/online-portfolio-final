0c877518da86c2b5fd69500473c2bb33b0d36ea6
Brix Aaron Monton <montonbrix123@gmail.com>
2024-03-28 15:47:55 +0800

    edit test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Lorem Ipsum <code>src/App.js</code> and save to reload.
+          Lorem Ipsum test<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
