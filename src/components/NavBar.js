d1a9fedaad952d4d92e3e03c733f3e662fba6a9e
Brix Aaron Monton <montonbrix123@gmail.com>
2024-04-02 11:56:10 +0800

    feat: added navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,28 @@
-import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import React from "react";
+import navIcon1 from '../assets/img/block-linkedin.png';
+import navIcon2 from '../assets/img/block-github.png';
+import navIcon3 from '../assets/img/block-fb.png';
+import logo from '../assets/img/logo.png';
 
 export const NavBar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="Logo" className="logo-img"/>
+        </Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className='navbar-link-home'></Nav.Link>
+            <Nav.Link href="#skills" className='navbar-link-skills'></Nav.Link>
+            <Nav.Link href="#projects" className='navbar-link-projects'></Nav.Link>
+          </Nav>          
+          <div className="social-icon">
+            <a href="https://www.linkedin.com/in/bsmonton/"><img src={navIcon1} alt="" /></a>
+            <a href="https://github.com/HumanSockPuppet"><img src={navIcon2} alt="" /></a>
+            <a href="https://facebook.com/handsomemechanic"><img src={navIcon3} alt="" /></a>
+          </div>     
         </Navbar.Collapse>
       </Container>
     </Navbar>
