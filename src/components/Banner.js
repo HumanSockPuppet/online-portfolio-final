12f53284f485adabc36981baac30cd8a35259cc5
Brix Aaron Monton <montonbrix123@gmail.com>
2024-04-04 22:00:59 +0800

    feat: Added Banner
    JIRA-5-Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/img/yoshi2.png";
+//import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/yoshi 3.png";
+import music from '../assets/music.mp3';
+import { useEffect, useRef } from "react";
+import { BsFillPauseFill, BsFillPlayFill } from "react-icons/bs";
 
-export const Banner = () => {
+export const Banner = () => { 
+  const [value, setValue] = useState(1);
+  const buttonText = value %2 ===0 ? <BsFillPauseFill className="music-icon"/> : <BsFillPlayFill className="music-icon"/>;  
+  const audio = useRef(new Audio(music));
+  audio.current.loop = true;
+
+  
+  useEffect(()=>{
+    if(value%2 === 0){
+      audio.current.play();
+    } else {
+      audio.current.pause();
+    }    
+  }, [value]);
+  
   return (
     <section className="banner" id="home">
       <Container>
+        <span className="tagline">WELCOME TO MY PORTFOLIO</span>
+      </Container>
+      <Container classname="container">
         <Row className="align-items-center">
-          <Col cs={12} md={6} xl={5}>
+          <Col cs={12} md={6} xl={6}>
             <img src={headerImg} alt="Header Img" />
           </Col>
 
-          <Col cs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my Portfolio, alhamdulillah</span>
-            <h1>{`Hi! I'm Brix Monton`} </h1>
+          <Col cs={12} md={6} xl={6}>
+            <h1>{`It's-a-me, BRIX!`} </h1>
             <p>
-              The href attribute requires a valid value to be accessible.
-              Provide a valid, navigable address as the href value. If you
-              cannot provide a valid href, but still need the element to
-              resemble a link, use a button and change it with appropriate
-              styles.
+            Welcome to my online portfolio, a neatly organized showcase of my projects. Here, you&apos;ll also find details about the technologies I specialize in. If you&apos;re interested in my skills, please don&apos;t hesitate to reach out via email! 
             </p>
-            <button onClick={() => console.log("connect")}>
-              Let&apos;s Connect
-              <ArrowRightCircle size={25} />
-            </button>
+            <button onClick={()=>setValue(value+1)}>
+              {buttonText} 
+            </button>            
           </Col>
         </Row>
       </Container>
